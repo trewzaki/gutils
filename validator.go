05da7ca03686597e08933d441033e553865415d1
Tananut Panyagosa <tongtananut@gmail.com>
2019-10-21 13:59:40 +0700

    Add Typevalidator function - Custom type with yourself and Modify commend in validator.go file

--- validator.go
+++ validator.go
@@ -4,12 +4,143 @@ import (
 	"encoding/json"
 )
 
-// >> Validation example:
-// if ok := gutils.IsString(dataMap, []string{"variable1", "variable2"}); !ok {
-// 	return gutils.ReturnValidationError()
-// }
+const (
+	// TypeString : string type symbol
+	TypeString = 1
 
-// IsString : Common Validator with string type value
+	// TypeInt : int type symbol
+	TypeInt = 2
+
+	// TypeInt8 : int8 type symbol
+	TypeInt8 = 3
+
+	// TypeInt16 : int16 type symbol
+	TypeInt16 = 4
+
+	// TypeInt32 : int32 type symbol
+	TypeInt32 = 5
+
+	// TypeInt64 : int64 type symbol
+	TypeInt64 = 6
+
+	// TypeUInt : uint type symbol
+	TypeUInt = 7
+
+	// TypeUInt8 : uint8 type symbol
+	TypeUInt8 = 8
+
+	// TypeUInt16 : uint16 type symbol
+	TypeUInt16 = 9
+
+	// TypeUInt32 : uint32 type symbol
+	TypeUInt32 = 10
+
+	// TypeUInt64 : uint64 type symbol
+	TypeUInt64 = 11
+
+	// TypeFloat32 : float32 type symbol
+	TypeFloat32 = 12
+
+	// TypeFloat64 : float64 type symbol
+	TypeFloat64 = 13
+
+	// TypeObject : map[string]interface{} type symbol
+	TypeObject = 14
+
+	// TypeArray : []interface{} type symbol
+	TypeArray = 15
+
+	// TypeArrayObject : []map[string]interface{} type symbol
+	TypeArrayObject = 16
+)
+
+// Typevalidator : Common validator with custom type value
+func Typevalidator(varType int, dataMap map[string]interface{}, varNames []string) bool {
+	for _, vn := range varNames {
+		switch varType {
+		case 1:
+			if _, ok := dataMap[vn].(string); !ok {
+				return false
+			}
+		case 2:
+			if _, ok := dataMap[vn].(int); !ok {
+				return false
+			}
+		case 3:
+			if _, ok := dataMap[vn].(int8); !ok {
+				return false
+			}
+		case 4:
+			if _, ok := dataMap[vn].(int16); !ok {
+				return false
+			}
+		case 5:
+			if _, ok := dataMap[vn].(int32); !ok {
+				return false
+			}
+		case 6:
+			if _, ok := dataMap[vn].(int64); !ok {
+				return false
+			}
+		case 7:
+			if _, ok := dataMap[vn].(uint); !ok {
+				return false
+			}
+		case 8:
+			if _, ok := dataMap[vn].(uint8); !ok {
+				return false
+			}
+		case 9:
+			if _, ok := dataMap[vn].(uint16); !ok {
+				return false
+			}
+		case 10:
+			if _, ok := dataMap[vn].(uint32); !ok {
+				return false
+			}
+		case 11:
+			if _, ok := dataMap[vn].(uint64); !ok {
+				return false
+			}
+		case 12:
+			if _, ok := dataMap[vn].(float32); !ok {
+				return false
+			}
+		case 13:
+			if _, ok := dataMap[vn].(float64); !ok {
+				return false
+			}
+		case 14:
+			for _, vn := range varNames {
+				if _, ok := dataMap[vn].(map[string]interface{}); !ok {
+					return false
+				}
+			}
+		case 15:
+			if _, ok := dataMap[vn].([]interface{}); !ok {
+				return false
+			} else if ok {
+				tmpMap := dataMap[vn].([]interface{})
+				if len(tmpMap) == 0 {
+					return false
+				}
+			}
+		case 16:
+			if _, ok := dataMap[vn].([]map[string]interface{}); !ok {
+				return false
+			} else if ok {
+				tmpMap := dataMap[vn].([]map[string]interface{})
+				if len(tmpMap) == 0 {
+					return false
+				}
+			}
+		}
+
+	}
+	return true
+}
+
+// IsString : Common validator with string type value
 func IsString(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(string); !ok {
@@ -20,7 +151,7 @@ func IsString(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsInt : Common Validator with int type value
+// IsInt : Common validator with int type value
 func IsInt(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(int); !ok {
@@ -31,7 +162,7 @@ func IsInt(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsUInt32 : Common Validator with uint32 type value
+// IsUInt32 : Common validator with uint32 type value
 func IsUInt32(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(uint32); !ok {
@@ -42,7 +173,7 @@ func IsUInt32(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsFloat : Common Validator with float type value
+// IsFloat : Common validator with float type value
 func IsFloat(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(float64); !ok {
@@ -53,7 +184,7 @@ func IsFloat(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsBool : Common Validator with boolean type value
+// IsBool : Common validator with boolean type value
 func IsBool(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(bool); !ok {
@@ -75,7 +206,7 @@ func IsInterface(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsObject : Common Validator with object type value
+// IsObject : Common validator with object type value
 func IsObject(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].(map[string]interface{}); !ok {
@@ -102,7 +233,7 @@ func IsArrayInterface(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsArray : Common Validator with array type value
+// IsArray : Common validator with array type value
 func IsArray(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].([]interface{}); !ok {
@@ -118,7 +249,7 @@ func IsArray(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// IsArrayObject : Common Validator with array object type value
+// IsArrayObject : Common validator with array object type value
 func IsArrayObject(dataMap map[string]interface{}, varNames []string) bool {
 	for _, vn := range varNames {
 		if _, ok := dataMap[vn].([]map[string]interface{}); !ok {
@@ -134,7 +265,7 @@ func IsArrayObject(dataMap map[string]interface{}, varNames []string) bool {
 	return true
 }
 
-// ReturnValidationError : Return error response when somethong wrong about validate variable
+// ReturnValidationError : Return error response when something wrong about validate variable
 func ReturnValidationError() []byte {
 	errRes := errorResponse{}
 	errRes.Success = false
