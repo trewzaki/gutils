ba6732831649ca694e8c9f5a1dc95dc2d0de3b1d
Tananut Panyagosa <tongtananut@gmail.com>
2021-03-23 16:43:12 +0700

    Add GetFGServiceName temporary function

--- service.go
+++ service.go
@@ -3,6 +3,7 @@ package gutils
 import (
 	"encoding/json"
 	"log"
+	"regexp"
 
 	"github.com/streadway/amqp"
 	"go.elastic.co/apm"
@@ -99,4 +100,48 @@ func ReturnServiceError() []byte {
 	resByte, _ := json.Marshal(errRes)
 
 	return resByte
+}
+
+func GetFGServiceName(exchangeName string) string {
+	if match, _ := regexp.MatchString("(fblfg_order)", exchangeName); match {
+		return "fblfg-order-service"
+	} else if match, _ := regexp.MatchString("(order|address|zipcode|sub-district|district|province)", exchangeName); match {
+		return "order-service"
+	} else if match, _ := regexp.MatchString("(content)", exchangeName); match {
+		return "content-service"
+	} else if match, _ := regexp.MatchString("(courier_partner)", exchangeName); match {
+		return "courier-partner"
+	} else if match, _ := regexp.MatchString("(courier)", exchangeName); match {
+		return "courier-service"
+	} else if match, _ := regexp.MatchString("(activity_logging)", exchangeName); match {
+		return "act-log-sev"
+	} else if match, _ := regexp.MatchString("(customer)", exchangeName); match {
+		return "cus-service"
+	} else if match, _ := regexp.MatchString("(dashboard)", exchangeName); match {
+		return "dashbrd-service"
+	} else if match, _ := regexp.MatchString("(employee_authentication)", exchangeName); match {
+		return "emp-auth-sev"
+	} else if match, _ := regexp.MatchString("(employee)", exchangeName); match {
+		return "emp-service"
+	} else if match, _ := regexp.MatchString("(invoice)", exchangeName); match {
+		return "invoice-service"
+	} else if match, _ := regexp.MatchString("(mail)", exchangeName); match {
+		return "mail-service"
+	} else if match, _ := regexp.MatchString("(payment)", exchangeName); match {
+		return "payment-service"
+	} else if match, _ := regexp.MatchString("(product|warehouse)", exchangeName); match {
+		return "product-service"
+	} else if match, _ := regexp.MatchString("(sale)", exchangeName); match {
+		return "sale-service"
+	} else if match, _ := regexp.MatchString("(shop)", exchangeName); match {
+		return "shop-service"
+	} else if match, _ := regexp.MatchString("(social)", exchangeName); match {
+		return "social-service"
+	} else if match, _ := regexp.MatchString("(support)", exchangeName); match {
+		return "support-service"
+	} else if match, _ := regexp.MatchString("(user)", exchangeName); match {
+		return "user-service"
+	}
+
+	return "none"
 }
